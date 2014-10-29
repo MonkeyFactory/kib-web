1ee865007dabaddfb34a004e2fbb6f30d71c05cd
Niklas Hedlund <nojan1989@gmail.com>
2014-10-29 14:26:15 +0100

    Added additional gallery rows for testing

--- pageroot/controllers/galleryPageController.js
+++ pageroot/controllers/galleryPageController.js
@@ -4,5 +4,31 @@ kibApp.controller('GalleryPageController', function($scope){
 					  "http://i232.photobucket.com/albums/ee195/CommanderSven/Warhammer%20Fantasy/Undead/MeatWagonpaint.jpg",
 					  "http://i35.photobucket.com/albums/d183/kin29rob/nvalhallans_wip_1_zpseea8e897.jpg",
 					  "http://i35.photobucket.com/albums/d183/kin29rob/Raptors%20Space%20Marines/raptor_test_1_zpsbc9bb88b.jpg",
-					  "http://i35.photobucket.com/albums/d183/kin29rob/Raptors%20Space%20Marines/motor_pool_1_zps844af263.jpg"];
+					  "http://i35.photobucket.com/albums/d183/kin29rob/Raptors%20Space%20Marines/motor_pool_1_zps844af263.jpg",
+					  "http://i766.photobucket.com/albums/xx301/Joppreck/P1010978_zps9c0541a1.jpg",
+					  "http://i766.photobucket.com/albums/xx301/Joppreck/71c4502e-12d5-4990-bb25-cd2d90573db4_zps937f8721.jpg",
+					  "http://i766.photobucket.com/albums/xx301/Joppreck/P1010709.jpg",
+					  "http://i766.photobucket.com/albums/xx301/Joppreck/P1010758.jpg"];
+					  
+					  $scope.images2 = ["http://i766.photobucket.com/albums/xx301/Joppreck/P1011095_zps880bb822.jpg",
+					  "http://i232.photobucket.com/albums/ee195/CommanderSven/Warhammer%20Fantasy/Undead/VampireDeathKnight1.jpg",
+					  "http://i232.photobucket.com/albums/ee195/CommanderSven/Warhammer%20Fantasy/Undead/MeatWagonpaint.jpg",
+					  "http://i35.photobucket.com/albums/d183/kin29rob/nvalhallans_wip_1_zpseea8e897.jpg",
+					  "http://i35.photobucket.com/albums/d183/kin29rob/Raptors%20Space%20Marines/raptor_test_1_zpsbc9bb88b.jpg",
+					  "http://i35.photobucket.com/albums/d183/kin29rob/Raptors%20Space%20Marines/motor_pool_1_zps844af263.jpg",
+					  "http://i766.photobucket.com/albums/xx301/Joppreck/P1010978_zps9c0541a1.jpg",
+					  "http://i766.photobucket.com/albums/xx301/Joppreck/71c4502e-12d5-4990-bb25-cd2d90573db4_zps937f8721.jpg",
+					  "http://i766.photobucket.com/albums/xx301/Joppreck/P1010709.jpg",
+					  "http://i766.photobucket.com/albums/xx301/Joppreck/P1010758.jpg"];
+					  
+					  $scope.images3 = ["http://i766.photobucket.com/albums/xx301/Joppreck/P1011095_zps880bb822.jpg",
+					  "http://i232.photobucket.com/albums/ee195/CommanderSven/Warhammer%20Fantasy/Undead/VampireDeathKnight1.jpg",
+					  "http://i232.photobucket.com/albums/ee195/CommanderSven/Warhammer%20Fantasy/Undead/MeatWagonpaint.jpg",
+					  "http://i35.photobucket.com/albums/d183/kin29rob/nvalhallans_wip_1_zpseea8e897.jpg",
+					  "http://i35.photobucket.com/albums/d183/kin29rob/Raptors%20Space%20Marines/raptor_test_1_zpsbc9bb88b.jpg",
+					  "http://i35.photobucket.com/albums/d183/kin29rob/Raptors%20Space%20Marines/motor_pool_1_zps844af263.jpg",
+					  "http://i766.photobucket.com/albums/xx301/Joppreck/P1010978_zps9c0541a1.jpg",
+					  "http://i766.photobucket.com/albums/xx301/Joppreck/71c4502e-12d5-4990-bb25-cd2d90573db4_zps937f8721.jpg",
+					  "http://i766.photobucket.com/albums/xx301/Joppreck/P1010709.jpg",
+					  "http://i766.photobucket.com/albums/xx301/Joppreck/P1010758.jpg"];
 });
