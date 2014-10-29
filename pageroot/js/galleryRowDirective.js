5f4150ebcbb4a335485e3b80506a52eeb30590d0
Niklas Hedlund <nojan1989@gmail.com>
2014-10-29 10:45:34 +0100

    Now uses css classes for image size change

--- pageroot/js/galleryRowDirective.js
+++ pageroot/js/galleryRowDirective.js
@@ -7,15 +7,21 @@ angular.module('kibGalleryModule', [])
 			restrict: 'C',
 			link : function(scope, element, attr) {
 				$timeout(function(){
+					//Set all images as unselected
+					element.find("li > img").each(function(index){
+						angular.element(this).addClass("unselectedImage");
+					});
+				
+					//Setup click handler
 					element.find("li > img").on("click", function(){
 						img = angular.element(this);
 						//alert(img.prop("tagName"));
 
 						element.find("li > img").each(function(index){
-						angular.element(this).css("height", "200px");
-					});
+							angular.element(this).removeClass("selectedImage").addClass("unselectedImage");
+						});
 				
-					img.css("height", "400px");
+					img.removeClass("unselectedImage").addClass("selectedImage");
 				});
 				}, 0);
 			}
