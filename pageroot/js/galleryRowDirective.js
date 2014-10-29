f5f9d01828fc832bce8c0987c0836866b57d4dd0
Niklas Hedlund <nojan1989@gmail.com>
2014-10-29 11:14:41 +0100

    Basic arrow key support

--- pageroot/js/galleryRowDirective.js
+++ pageroot/js/galleryRowDirective.js
@@ -2,11 +2,19 @@ angular.module('kibGalleryModule', [])
 	.controller('galleryController', function(){
 		alert("Hello");
 	})
-	.directive('galleryrow', function($timeout){
+	.directive('galleryrow', function($timeout, $document){
 		return {
 			restrict: 'C',
 			link : function(scope, element, attr) {
 				$timeout(function(){
+					var selectImage = function(img){
+						element.find("li > img").each(function(index){
+							angular.element(this).removeClass("selectedImage").addClass("unselectedImage");
+						});
+				
+						img.removeClass("unselectedImage").addClass("selectedImage");
+					}
+				
 					//Set all images as unselected
 					element.find("li > img").each(function(index){
 						angular.element(this).addClass("unselectedImage");
@@ -16,13 +24,29 @@ angular.module('kibGalleryModule', [])
 					element.find("li > img").on("click", function(){
 						img = angular.element(this);
 						//alert(img.prop("tagName"));
+						selectImage(img);
+						
+					});
 
-						element.find("li > img").each(function(index){
-							angular.element(this).removeClass("selectedImage").addClass("unselectedImage");
-						});
-				
-					img.removeClass("unselectedImage").addClass("selectedImage");
-				});
+					$document.on("keydown", function(event){
+						var selectedImage = element.find(".selectedImage");
+						if(!selectedImage)
+							return;
+							
+						var currentLI = selectedImage.parent();
+						switch(event.which){
+							case 37:
+								//go left
+								var newLI = currentLI.prev();
+								break;
+							case 39:
+								//go right
+								var newLI = currentLI.next();
+								break;
+						}
+						
+						selectImage(newLI.find("img"));
+					});
 				}, 0);
 			}
 		}
