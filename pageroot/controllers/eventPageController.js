f2f69c3aab4f93f19c700c858e7e6599cb4ad026
Niklas Hedlund <nojan1989@gmail.com>
2014-11-06 10:44:02 +0100

    Added event display animation

--- pageroot/controllers/eventPageController.js
+++ pageroot/controllers/eventPageController.js
@@ -7,13 +7,20 @@ kibApp.controller('EventPageController', function($scope, kibservice){
 	 
 	$scope.alertOnEventClick = function(event, allDay, jsEvent, view){
 		$scope.selectedEvent = event;
+		//$scope.eventCalendar.fullCalendar("option", "height", "10");
+		angular.element("#calendarcontainer").animate({height: 200}, 200);
 	};
+	 
+	$scope.closeEvent = function(){
+		$scope.selectedEvent = undefined;
+		angular.element("#calendarcontainer").animate({height: 620}, 200);
+	}
 	 
 	$scope.uiConfig = {
       calendar:{
 		lang: "sv",
         editable: false,
-		height:500,
+		height:600,
         header:{
           left: 'title',
           center: '',
