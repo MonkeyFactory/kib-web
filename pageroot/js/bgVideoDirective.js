5b9062d2cf83d4dad5148924d1eafdced9a7ea09
Niklas Hedlund <nojan1989@gmail.com>
2014-11-17 13:52:57 +0100

    Implemented playlist playback functionality

--- pageroot/js/bgVideoDirective.js
+++ pageroot/js/bgVideoDirective.js
@@ -2,12 +2,38 @@ kibApp.directive('bgVideo', function($document){
 		return {
 			scope: {
 				videos: '=',
-				
 			},
 			restrict: 'E',
-			template: '<video />',
+			template: '<video muted/>',
 			link: function(scope, element, attr) {
-				console.log(scope.videos);
+				var nextVideo = function(){
+					if(scope.currentVideo != undefined){
+						scope.currentVideo += 1;
+						if(scope.currentVideo > scope.videos.length - 1){
+							scope.currentVideo = 0;
+						}
+					}else{
+						scope.currentVideo = 0;
+					}
+					
+					console.log("Playing video " + scope.currentVideo + " = " + scope.videos[scope.currentVideo]);
+				
+					video = element.find("video");
+					videofile = "media/video/" + scope.videos[scope.currentVideo];
+					video.html(angular.element('<source src="' + videofile + '.mp4" type="video/mp4"/>'));
+					video.append(angular.element('<source src="' + videofile + '.webm" type="video/webm"/>'));
+					video.append(angular.element('<source src="' + videofile + '.ogv" type="video/ogg"/>'));
+					
+					video.get(0).play();
+				}
+			
+				element.find("video").on("ended", function(){
+					console.log("Video ended");
+					nextVideo();
+				});
+				
+				//Trigger playback start
+				nextVideo();
 			}
 		}
 	});
