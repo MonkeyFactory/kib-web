e5fad39812797486fc8a623a45b7c9648cda4e35
Niklas Hedlund <nojan1989@gmail.com>
2014-11-18 08:29:55 +0100

    Set videos on controller level

--- pageroot/controllers/startPageController.js
+++ pageroot/controllers/startPageController.js
@@ -1,4 +1,6 @@
 kibApp.controller('StartPageController', function($scope){
+	$scope.videos = ['Example', 'RollingDice', 'AnotherVideo'];
+
 	$scope.messages = [{Title: "Fun place to be 1", Text: "It is, isn't it?"},
 					   {Title: "Fun place to be 2", Text: "It is, isn't it?"},
 					   {Title: "Fun place to be 3", Text: "It is, isn't it?"}];
