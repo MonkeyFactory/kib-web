c404ca21a18bb5b7a41faf41b9a1f795a6f9d25c
Niklas Hedlund <nojan1989@gmail.com>
2015-01-09 15:35:52 +0100

    Changed video filenames to correct case

--- pageroot/controllers/startPageController.js
+++ pageroot/controllers/startPageController.js
@@ -1,5 +1,5 @@
 kibApp.controller('StartPageController', function($scope){
-	$scope.videos = ['Example', 'RollingDice', 'AnotherVideo'];
+	$scope.videos = ['example', 'RollingDice'];
 
 	$scope.messages = [{Title: "Fun place to be 1", Text: "It is, isn't it?"},
 					   {Title: "Fun place to be 2", Text: "It is, isn't it?"},
