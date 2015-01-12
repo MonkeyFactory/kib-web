782d2a8a7adf5c94d582d01c7ce4ccd80564988e
Niklas Hedlund <nojan1989@gmail.com>
2015-01-12 16:56:02 +0100

    Added click handler to selected league, changed to UTF8

--- pageroot/controllers/leagueListPageController.js
+++ pageroot/controllers/leagueListPageController.js
@@ -1,3 +1,7 @@
 kibApp.controller('LeagueDetailsPageController', function($scope, kibservice){
 	$scope.leagues = kibservice.GetLeagues();
+	
+	$scope.onSelectedLeague = function(league){
+		console.log(league.Name);
+	}
 });
