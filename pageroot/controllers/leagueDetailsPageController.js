84d2e7159527cabb81c89bfdf6d72c2c98fbb890
Niklas Hedlund <nojan1989@gmail.com>
2015-01-13 16:19:07 +0100

    Added hardcoded leaderboard

--- pageroot/controllers/leagueDetailsPageController.js
+++ pageroot/controllers/leagueDetailsPageController.js
@@ -2,8 +2,21 @@ kibApp.controller('LeagueDetailsPageController', function($scope, $window){
 	$scope.minimized = false;
 	$scope.tab = "leaderboard";
 	
-	 angular.element($window).bind("scroll", function(e) {
+	angular.element($window).bind("scroll", function(e) {
        $scope.minimized = true;
 	   $scope.$apply();
 	});
+	
+	$scope.leaderboard = [{Name: "Johan", Wins: "5", Draws: "4", Score: "140"},
+						  {Name: "Johan", Wins: "5", Draws: "4", Score: "140"},
+						  {Name: "Johan", Wins: "5", Draws: "4", Score: "140"},
+						  {Name: "Johan", Wins: "5", Draws: "4", Score: "140"},
+						  {Name: "Johan", Wins: "5", Draws: "4", Score: "140"},
+						  {Name: "Johan", Wins: "5", Draws: "4", Score: "140"},
+						  {Name: "Johan", Wins: "5", Draws: "4", Score: "140"},
+						  {Name: "Johan", Wins: "5", Draws: "4", Score: "140"},
+						  {Name: "Johan", Wins: "5", Draws: "4", Score: "140"},
+						  {Name: "Johan", Wins: "5", Draws: "4", Score: "140"},
+						  {Name: "Johan", Wins: "5", Draws: "4", Score: "140"},
+						  {Name: "Johan", Wins: "5", Draws: "4", Score: "140"},];
 });
