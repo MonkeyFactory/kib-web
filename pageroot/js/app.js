60715aa85771eb5724ccb9ee4c3ab93f6b664020
Niklas Hedlund <nojan1989@gmail.com>
2015-01-15 10:10:37 +0100

    Fixed typo in route specification

--- pageroot/js/app.js
+++ pageroot/js/app.js
@@ -52,7 +52,7 @@ var kibApp = angular.module('kibApp', ['ngRoute', 'ngResource', 'ngAnimate', 'an
 			controller: 'LeagueDetailsPageController'
 		})
 		
-		.when('leagues/:leagueId/reportmatch', {
+		.when('/leagues/:leagueId/reportmatch', {
 			templateUrl: 'templates/leagueReportMatchTemplate.html',
 			controller: 'LeagueReportMatchController'
 		})
@@ -66,4 +66,28 @@ var kibApp = angular.module('kibApp', ['ngRoute', 'ngResource', 'ngAnimate', 'an
 		return function(val) {
 			return $sce.trustAsHtml(val);
 		};
-	});;
+	});
+	
+/* 	.run(function($rootScope){
+		$rootScope.$on('$stateChangeStart',function(event, toState, toParams, fromState, fromParams){
+		  console.log('$stateChangeStart to '+toState.to+'- fired when the transition begins. toState,toParams : \n',toState, toParams);
+		});
+		$rootScope.$on('$stateChangeError',function(event, toState, toParams, fromState, fromParams){
+		  console.log('$stateChangeError - fired when an error occurs during transition.');
+		  console.log(arguments);
+		});
+		$rootScope.$on('$stateChangeSuccess',function(event, toState, toParams, fromState, fromParams){
+		  console.log('$stateChangeSuccess to '+toState.name+'- fired once the state transition is complete.');
+		});
+		// $rootScope.$on('$viewContentLoading',function(event, viewConfig){
+		//   // runs on individual scopes, so putting it in "run" doesn't work.
+		//   console.log('$viewContentLoading - view begins loading - dom not rendered',viewConfig);
+		// });
+		$rootScope.$on('$viewContentLoaded',function(event){
+		  console.log('$viewContentLoaded - fired after dom rendered',event);
+		});
+		$rootScope.$on('$stateNotFound',function(event, unfoundState, fromState, fromParams){
+		  console.log('$stateNotFound '+unfoundState.to+'  - fired when a state cannot be found by its name.');
+		  console.log(unfoundState, fromState, fromParams);
+		});
+	}); */
