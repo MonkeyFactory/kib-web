c77c4e62f07d53afb178b253a2065ad413914f5e
Niklas Hedlund <nojan1989@gmail.com>
2015-01-15 11:35:29 +0100

    Added loading directive

---
+++ pageroot/js/loadingDirective.js
@@ -0,0 +1,18 @@
+kibApp.directive('loading', function($document){
+		return {
+			restrict: 'E',
+			transclude: true,
+			template: '<div class="loading-container">\
+						<div class="loading-text" ng-transclude></div>\
+						<div class="spinner-cube">\
+							<div>\
+								<div class="dot dot-one"></div>\
+							</div>\
+							<div>\
+								<div class="dot dot-two1"></div>\
+								<div class="dot dot-two2"></div>\
+							</div>\
+						</div>\
+					</div>'
+		}
+	});
