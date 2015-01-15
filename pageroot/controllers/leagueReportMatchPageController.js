72ddbf6c074e76e2e2d34a90d9d8dcc7ef2000b4
Niklas Hedlund <nojan1989@gmail.com>
2015-01-15 10:48:00 +0100

    Report match template and controller

--- pageroot/controllers/leagueReportMatchPageController.js
+++ pageroot/controllers/leagueReportMatchPageController.js
@@ -1,3 +1,19 @@
-kibApp.controller('LeagueReportMatchController', function($scope, kibservice, $location){
+kibApp.controller('LeagueReportMatchController', function($scope, $routeParams, kibservice, adminservice){
+	adminservice.GetAuthInfo(function(auth){
+		if(auth.authlevel == 0){
+			//not logged in
+			//$location.url("/forum/ucp.php?mode=login&redirect=%23%2Fleagues%2F" + $routeParams.leagueId + "%2Freportmatch");
+		}else{
+			$scope.auth = auth;
+		}
+	});
 	
+	//Temp
+	$scope.auth = {Username: "nojan"}
+	
+	$scope.match = {};
+	
+	$scope.reportMatch = function(){
+	
+	}
 });
