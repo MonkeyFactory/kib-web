f8c3c083fbddb80a629add241e0c65e6586884da
Niklas Hedlund <nojan1989@gmail.com>
2015-01-15 15:52:41 +0100

    Report league match now checks auth with server

--- pageroot/controllers/leagueReportMatchPageController.js
+++ pageroot/controllers/leagueReportMatchPageController.js
@@ -1,15 +1,12 @@
-kibApp.controller('LeagueReportMatchController', function($scope, $routeParams, kibservice, adminservice){
+kibApp.controller('LeagueReportMatchController', function($scope, $routeParams, $location, kibservice, adminservice){
 	adminservice.GetAuthInfo(function(auth){
 		if(auth.authlevel == 0){
 			//not logged in
-			//$location.url("/forum/ucp.php?mode=login&redirect=%23%2Fleagues%2F" + $routeParams.leagueId + "%2Freportmatch");
+			$location.url("/forum/ucp.php?mode=login&redirect=%23%2Fleagues%2F" + $routeParams.leagueId + "%2Freportmatch");
 		}else{
 			$scope.auth = auth;
 		}
 	});
-	
-	//Temp
-	$scope.auth = {Username: "nojan"}
 	
 	$scope.match = {};
 	
