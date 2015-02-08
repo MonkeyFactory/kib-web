920a3d09f13a9972b72807005cf75977d386faa4
Niklas Hedlund <nojan1989@gmail.com>
2015-02-08 18:14:21 +0100

    Start page slide texts should be defined in template

--- pageroot/controllers/startPageController.js
+++ pageroot/controllers/startPageController.js
@@ -1,7 +1,3 @@
 kibApp.controller('StartPageController', function($scope){
 	$scope.videos = ['RollingDice', 'MovingDogs'];
-
-	$scope.messages = [{Title: "Trevligt folk", Text: "Upplev en trevlig gemenskap bland likasinnade"},
-					   {Title: "God mat", Text: "Det bästa snabbmatsborlänge kan uppbringa"},
-					   {Title: "Mycket skratt", Text: "Tro mig! Vi gör inget annat ;)"}];
 });
