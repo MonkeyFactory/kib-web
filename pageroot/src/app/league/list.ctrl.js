5b63f2abc6fdf099c825b332240717208e67b307
Niklas Hedlund <nojan1989@gmail.com>
2015-07-10 12:06:36 +0200

    Hide message telling people to join league if non are active

--- pageroot/src/app/league/list.ctrl.js
+++ pageroot/src/app/league/list.ctrl.js
@@ -3,6 +3,17 @@
 angular.module('kibApp').controller('LeagueListPageController', function($scope, kibservice, $location){
 	$scope.leagues = kibservice.GetLeagues();
 	
+	$scope.allLeaguesIsInactive = function(){
+		for(var i = 0; i < $scope.leagues.length; i++){
+			var league = $scope.leagues[i];
+			if(!league.EndDate || moment(league.EndDate) < moment()){
+				return false;	
+			}
+		}
+		
+		return true;
+	}
+	
 	$scope.onSelectedLeague = function(league){
 		$location.path('/leagues/' + league.leagueId);
 	};
