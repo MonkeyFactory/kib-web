7686c6354ae7eb756266fc524ee202964cecfe3f
Niklas Hedlund <nojan1989@gmail.com>
2015-07-14 22:24:30 +0200

    Began work on tournament admin main page

--- pageroot/src/admin/tournament/admin.ctrl.js
+++ pageroot/src/admin/tournament/admin.ctrl.js
@@ -1,5 +1,30 @@
 'use strict';
 
 angular.module('kibAdmin').controller('AdminTournament', function($stateParams, $scope){
-	alert($stateParams.tournamentId);
+	
+	$scope.rounds = [
+		{
+			name: 'Round 1',
+			matchups: [
+				{
+					table: 'Table 1',
+					player1: 'Nojan',
+					player2: 'Robin'
+				},
+				{
+					table: 'Table 2',
+					player1: 'Johan',
+					player2: 'Kim'
+				}
+			]
+		}
+	];
+	
+	$scope.createNewMatch = function(){
+		$scope.rounds.push({name: 'Apa'});
+	}
+	
+	$scope.allMatchesReported = function(){
+		return true;	
+	};
 });
